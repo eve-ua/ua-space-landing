a96a10538c952dae896e2dc9e3a648f880d962cf
Your Name <you@example.com>
2024-12-22 16:18:15 +0100

    :bug: fix GitHub Pages pipeline

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
-  ssr: false,
+  ssr: true,
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   modules: [
     'nuxt-gtag',
     '@nuxtjs/tailwindcss',
